debd668b52cb21ed893a27d782a6589d600ead80
Marcus Gartner <magartner@gmail.com>
2011-10-14 21:01:19 -0400

    fixed typo in curl output

--- curl_catcher.sh
+++ curl_catcher.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-curl --silent -m 300 https://stream.twitter.com/1/statuses/sample.json -u$TWITTER_USER:$TWITTER_PASS >> /home/ec2-user/twtr/data/$(date +%Y%m%d-%H).twtr
+curl --silent -m 300 https://stream.twitter.com/1/statuses/sample.json -u$TWITTER_USER:$TWITTER_PASS >> /home/ec2-user/tweek/data/$(date +%Y%m%d-%H).twtr
