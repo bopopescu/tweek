08c35476b10beb0336c51902d050d49fd260d9e1
Marcus Gartner <magartner@gmail.com>
2011-10-26 13:32:17 -0400

    updated bootstrap script

--- build_index/bootstrap.sh
+++ build_index/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install rubygems
+sudo apt-get -y safe-upgrade
+sudo apt-get -y install unzip build-essential git-core ia32-libs
+sudo apt-get -y -t universe install ruby rubygems ruby-dev
+
 sudo gem install crack --source http://rubygems.org
 sudo gem install whatlanguage
 
