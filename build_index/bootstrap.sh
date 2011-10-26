bd0b5a94db3da8cb9005dfc47a79188c89d93a26
Marcus Gartner <magartner@gmail.com>
2011-10-26 14:34:46 -0400

    new bootstrap script to try

--- build_index/bootstrap.sh
+++ build_index/bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y safe-upgrade
-sudo apt-get -y install unzip build-essential git-core ia32-libs
-sudo apt-get -y -t universe install ruby rubygems ruby-dev
+#sudo apt-get -y safe-upgrade
+#sudo apt-get -y install unzip build-essential git-core ia32-libs
+#sudo apt-get -y -t universe install ruby rubygems ruby-dev
 
-sudo gem install hoe
+sudo apt-get update
+sudo apt-get install ruby1.8-dev
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
+tar xvfz rubygems-1.8.10.tgz
+cd rubygems-1.8.10 && sudo ruby setup.rb
+
 sudo gem install crack --source http://rubygems.org
 sudo gem install whatlanguage
 
