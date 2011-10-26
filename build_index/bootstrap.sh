6e2a19bfc87972de16057ab26378b41abcfbeeb8
Marcus Gartner <magartner@gmail.com>
2011-10-26 13:49:47 -0400

    added hoe gem to bootstrap

--- build_index/bootstrap.sh
+++ build_index/bootstrap.sh
@@ -5,6 +5,7 @@ sudo apt-get -y safe-upgrade
 sudo apt-get -y install unzip build-essential git-core ia32-libs
 sudo apt-get -y -t universe install ruby rubygems ruby-dev
 
+sudo gem install hoe
 sudo gem install crack --source http://rubygems.org
 sudo gem install whatlanguage
 
