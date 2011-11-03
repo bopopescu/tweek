a595dc350985719e0bc5a33e4e1927b095ad2803
Marcus Gartner <magartner@gmail.com>
2011-11-02 21:50:24 -0400

    finally found correct bootstrap combo

--- build_index/bootstrap.sh
+++ build_index/bootstrap.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-#sudo apt-get -y safe-upgrade
-#sudo apt-get -y install unzip build-essential git-core ia32-libs
-#sudo apt-get -y -t universe install ruby rubygems ruby-dev
+sudo apt-get install irb1.8 libreadline-ruby1.8 libruby libruby1.8 rdoc1.8 ruby ruby1.8 ruby1.8-dev -qq
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
+tar xvfz rubygems-1.8.10.tgz
+cd rubygems-1.8.10
+sudo ruby setup.rb
 
-sudo apt-get update
-sudo apt-get -y install ruby1.8-dev
-sudo apt-get -y install rubygems
-#wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
-#tar xvfz rubygems-1.8.10.tgz
-#cd rubygems-1.8.10 && sudo ruby setup.rb
-
-sudo gem install crack --source http://rubygems.org
-sudo gem install whatlanguage --source http://rubygems.org
+sudo gem1.8 install hoe --source http://rubygems.org
+sudo gem1.8 install crack --source http://rubygems.org
+sudo gem1.8 install whatlanguage --source http://rubygems.org
 
 exit
