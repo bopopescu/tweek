a4bd06dd9bd1a1d9245400339a2d82cdd87adb12
Tala Huhe <thuhe@cs.brown.edu>
2011-12-17 18:37:33 -0500

    Writeup

--- lib/netderp.py
+++ lib/netderp.py
@@ -2,7 +2,9 @@ import csv
 import boto
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+
 from derpkeys import *
+from timederp import *
 
 s3_comparison_bucket = "twtr-compare"
 sdb_stock_domain = "stock-data"
@@ -27,4 +29,24 @@ def sdb_get_domain():
     sdb = boto.connect_sdb(ACCESS_KEY, SECRET_KEY)
     domain = sdb.get_domain(sdb_stock_domain)
     return domain
+
+def sdb_parse_name(item_name):
+    index = item_name.find("-")
+    return (item_name[:index], item_name[index+1:])
+
+def sdb_make_name(ticker, time):
+    return "%s-%s" % (ticker, time2timestamp(time))
+
+def sdb_get_item(domain, ticker, time):
+    name = sdb_make_name(ticker, time)
+    return domain.get_item(name)
+
+def sdb_ensure_item(domain, ticker, time):
+    trial = sdb_get_item(domain, ticker, time)
+    if trial:
+        return trial
+    else:
+        return domain.new_item(sdb_make_name(ticker, time))
+
+
 
