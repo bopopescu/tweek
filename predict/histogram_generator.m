d46bcf53a43edbe8089da2733a4f5b65ce1c09a3
Tala Huhe <thuhe@cs.brown.edu>
2011-12-17 19:26:22 -0500

    Histogram changes

--- predict/histogram_generator.m
+++ predict/histogram_generator.m
@@ -1,11 +1,16 @@
 function histogram_generator
-    
+
 top5 = csvread('top_5.csv', 1, 4);
 top10 = csvread('top_10.csv', 1, 4);
 top15 = csvread('top_15.csv', 1, 4);
 
-bins = 0:.0005:.01;
-M = [bins; hist(top5, bins); hist(top10, bins); hist(top15, bins)]'
+bins = 0:.0005:.01+0.0005;
+
+top5_h = histc(top5, bins);
+top10_h = histc(top10, bins);
+top15_h = histc(top15, bins);
+
+M = horzcat(bins', top5_h, top5_h / sum(top5_h), top10_h, top10_h / sum(top10_h), top15_h, top15_h / sum(top15_h))
 
 csvwrite('differences.csv', M)
 
